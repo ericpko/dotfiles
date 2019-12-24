7512c192ae8a355e34bb419f55a03ab02e7655f8
Eric K <eric.koehli@mail.utoronto.ca>
2019-12-24 13:26:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-git clone --bare git@bitbucket.org:durdn/cfg.git $HOME/.cfg
+git clone --bare https://github.com/ericpko/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
